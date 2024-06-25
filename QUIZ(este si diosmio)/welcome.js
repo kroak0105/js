e3cdfaae68b92a8efe10d1beda04ba750d6d093d
kroak0105 <164654578+kroak0105@users.noreply.github.com>
2024-06-25 17:36:59 -0400

    Add files via upload

---
+++ QUIZ(este si diosmio)/welcome.js
@@ -0,0 +1,10 @@
+function entrar() {
+    document.getElementById("button");
+    window.location = 'quiz.html';
+}
+
+function reintentar(){
+    document.getElementById();
+    window.location = 'quiz.html';
+}
+
