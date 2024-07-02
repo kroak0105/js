d6f61f51ef1ad7aa9193c72259f096747585c91d
kroak0105 <164654578+kroak0105@users.noreply.github.com>
2024-07-01 21:23:43 -0400

    Delete Quiz directory

--- Quiz/data/ppreguntas.js
+++
@@ -1,4 +0,0 @@
-import { preguntas } from "../models/preguntas.js";
-import { data } from "./data.js";
-
-export const newPregunta = data.map(Preguntas => new preguntas(Preguntas.pregunta, Preguntas.elecciones, Preguntas.respuesta));
--- Quiz/wlcm.js
+++
@@ -1,9 +0,0 @@
-/*function entrar2() {
-    document.getElementById("button");
-    window.location = "quiz.html";
-}
-/*function volver(){
-    document.getElementById("btn2")
-    window.location="inicio.html";
-}*/
-
